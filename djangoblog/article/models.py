de61384dddb06f14f150e0d6c75b56fc7a016237
Sergeeva Larisa <mindzu@mail.ru>
2024-06-21 16:04:25 +0500

    first base

--- djangoblog/article/models.py
+++ djangoblog/article/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Article(models.Model):
 
     name = models.CharField(max_length=200)
-    body = models.TextField
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
