c3c5668fcf32885e5bdb734a8f8661a4158c0277
Sergeeva Larisa <mindzu@mail.ru>
2024-06-23 22:58:48 +0500

    create admin

--- djangoblog/article/admin.py
+++ djangoblog/article/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'body']
+    list_display = ('name', 'created_at')
+    list_filter = (('created_at', admin.DateFieldListFilter),)
